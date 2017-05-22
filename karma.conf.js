6b98e63f13fab803c49d43d56ad74910baf0c456
Julian Heeb <julian.heeb@scs.ch>
2017-05-22 13:24:11 +0200

    make it packageable

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -8,37 +10,41 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-remap-istanbul'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      {pattern: './scripts/test.ts', watched: false}
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      './scripts/test.ts': ['@angular/cli']
     },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+    remapIstanbulReporter: {
+      reports: {
+        html: 'coverage',
+        lcovonly: './coverage/coverage.lcov'
+      }
     },
     angularCli: {
+      config: './.angular-cli.json',
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+      ? ['dots', 'karma-remap-istanbul']
+      : ['dots'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    mime: { 'text/x-typescript': ['ts','tsx'] },
+    client: { captureConsole: true }
   });
 };
--- scripts/test.ts
+++ scripts/test.ts
@@ -1,4 +1,4 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import '../demo/src/polyfills.ts';
 
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
@@ -12,12 +12,14 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+import './matchers';
+
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
 declare var require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = Function.prototype;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -25,8 +27,11 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+let context = require.context('../demo/src', true, /\.spec\.ts/);
 // And load the modules.
 context.keys().map(context);
+
+let context2 = require.context('../src/spec', true, /\.spec\.ts/);
+context2.keys().map(context2);
 // Finally, start Karma to run the tests.
 __karma__.start();
